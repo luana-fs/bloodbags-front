f2526fcbd355da87aa145123f9f8bf7d5849e5b5
Eduardo_LopesDoAmara <eduardo.lopes.amaral@outlook.com>
2023-07-06 11:06:53 -0300

    addBagComponent start

---
+++ bloodbags/src/components/AddBagForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+      <div class="add-bag-form">
+        <form action="Post">
+            <div>
+                <input type="text">   
+        
+                <input type="text">   
+        
+                <input type="text">
+            </div>   
+        </form>
+        <PrimaryButton text="Adicionar"></PrimaryButton>
+      </div>
+    </main>
+  </template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+
+</style>
